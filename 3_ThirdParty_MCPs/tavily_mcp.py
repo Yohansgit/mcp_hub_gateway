b7e68df1550b8ea139bea77d8027cecb11c3bcdd
Yohansgit <geapeac@outlook.com>
2026-05-23 13:52:16 -0400

    Test and run tavily_search tool integration

--- 3_ThirdParty_MCPs/tavily_mcp.py
+++ 3_ThirdParty_MCPs/tavily_mcp.py
@@ -2,6 +2,8 @@ from langchain_mcp_adapters.client import MultiServerMCPClient, Connection
 import asyncio
 import os
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
 async def main():
@@ -17,7 +19,15 @@ async def main():
 
     # List the tools
     tools = await client.get_tools()
-    print("Available tools:", tools)
+    for tool in tools:
+        print("Available tool:", tool.name)
+
+# test tavily_search tool
+    tool_search= [tool for tool in tools if tool.name == "tavily_search"][0]
+    result = await tool_search.ainvoke({"query": "What is the capital of France?"})
+    print("Tool result:", result)
+
+    # call tool
 
 if __name__ == "__main__":
     asyncio.run(main()) 
